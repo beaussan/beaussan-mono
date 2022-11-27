52e7935ce9531f39467a385e0c014cad661aea14
Nicolas Beaussart <nic.beaussart@gmail.com>
2022-11-27 14:30:34 +0100

    dash

--- libs/internal-generator/src/utils/consts.ts
+++ libs/internal-generator/src/utils/consts.ts
@@ -1,5 +1,5 @@
 export const tagTypeList = ['feature', 'ui', 'util'] as const;
 export type TagType = typeof tagTypeList[number];
 
-export const tagScopeList = ['landing-page', 'shared'] as const;
+export const tagScopeList = ['dash', 'shared'] as const;
 export type TagScope = typeof tagScopeList[number];
