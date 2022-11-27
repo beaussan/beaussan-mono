24e39446a34c2b07ba1ef1f1ca4c5f490a6eaf16
Nicolas Beaussart <nic.beaussart@gmail.com>
2022-11-27 01:03:39 +0100

    move generator name

--- apps/internal-generator-e2e/jest.config.ts
+++ apps/internal-generator-e2e/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'internal-generators-e2e',
+  displayName: 'internal-generator-e2e',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/internal-generators-e2e',
+  coverageDirectory: '../../coverage/apps/internal-generator-e2e',
 };
