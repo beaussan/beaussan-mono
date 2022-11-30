e0c8311bb546f716ca1da18e4040ea5271961f25
Nicolas Beaussart <nic.beaussart@gmail.com>
2022-11-30 19:26:58 +0100

    skip cypress test

--- apps/new-tab-page-e2e/src/e2e/app.cy.ts
+++ apps/new-tab-page-e2e/src/e2e/app.cy.ts
@@ -3,7 +3,7 @@ import { getGreeting } from '../support/app.po';
 describe('new-tab-page', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
+  it.skip('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
     cy.login('my-email@something.com', 'myPassword');
 
