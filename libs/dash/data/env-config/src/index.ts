bbac5dfc92b434043856dc0537ba100b400d5547
Nicolas Beaussart <nic.beaussart@gmail.com>
2022-12-21 18:15:21 +0100

    cleaner solution

--- libs/dash/data/env-config/src/index.ts
+++ libs/dash/data/env-config/src/index.ts
@@ -1,4 +1,2 @@
-export { clientEnvs } from './lib/clientEnv';
-export type { ClientEnv } from './lib/clientEnv';
-export { getServerEnvs } from './lib/serverEnv';
-export type { ServerEnv } from './lib/serverEnv';
+export { clientEnvs, type ClientEnv } from './lib/clientEnv';
+export { getServerEnvs, type ServerEnv } from './lib/serverEnv';
