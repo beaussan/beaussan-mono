dbfe68d4c18eb4fdbb933a29c6cdf5ea230a1eb2
Nicolas Beaussart <nic.beaussart@gmail.com>
2022-12-22 23:48:38 +0100

    update tsconfig

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from '@storybook/core-common';
 
 export const rootMain: StorybookConfig = {
   stories: [],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-a11y'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+  ],
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
 
