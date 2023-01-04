5149e2f19d14ccb7145d68e6746701f1561c387a
Nicolas Beaussart <nic.beaussart@gmail.com>
2023-01-04 15:33:48 +0100

    apply storybook migration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,17 @@
 import type { StorybookConfig } from '@storybook/core-common';
-
 export const rootMain: StorybookConfig = {
   stories: [],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
-  ],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-a11y']
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
 
   //   // Return the altered config
   //   return config;
   // },
+};
+
+export const framework = '@storybook/react';
+export const framework = {
+  name: '@storybook/react-webpack5',
+  options: {}
 };
