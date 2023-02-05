59ec6cb0f0446a10b5330bb18ccd87bfce25d7c0
Nicolas Beaussart <nic.beaussart@gmail.com>
2023-02-05 16:45:34 +0100

    update types

--- libs/dash/feature/bookmark-list/src/lib/requests.generated.ts
+++ libs/dash/feature/bookmark-list/src/lib/requests.generated.ts
@@ -2,78 +2,135 @@ import * as Types from '@beaussan/dash/data/hasura-codegen-types';
 
 import gql from 'graphql-tag';
 import * as Urql from 'urql';
-import { graphql, ResponseResolver, GraphQLRequest, GraphQLContext } from 'msw'
+import { graphql, ResponseResolver, GraphQLRequest, GraphQLContext } from 'msw';
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-export type BookmarkItemFragment = { __typename?: 'Bookmarks', id: string, link: string, faviconUrl?: string | null, displayName: string, position?: number | null };
+export type BookmarkItemFragment = {
+  __typename?: 'Bookmarks';
+  id: string;
+  link: string;
+  faviconUrl?: string | null;
+  displayName: string;
+  position?: number | null;
+};
 
-export type TraefikRoutesFragment = { __typename?: 'TraefikRoutes', faviconUrl?: string | null, lastSeenAlive: string, isUp: boolean, name: string, friendlyName?: string | null, link: string };
+export type TraefikRoutesFragment = {
+  __typename?: 'TraefikRoutes';
+  faviconUrl?: string | null;
+  lastSeenAlive: string;
+  isUp: boolean;
+  name: string;
+  friendlyName?: string | null;
+  link: string;
+};
 
-export type GetListOfBookmarksQueryVariables = Types.Exact<{ [key: string]: never; }>;
+export type GetListOfBookmarksQueryVariables = Types.Exact<{
+  [key: string]: never;
+}>;
 
-
-export type GetListOfBookmarksQuery = { __typename?: 'query_root', bookmarks: Array<{ __typename?: 'Bookmarks', id: string, link: string, faviconUrl?: string | null, displayName: string, position?: number | null }>, traefikRoutes: Array<{ __typename?: 'TraefikRoutes', faviconUrl?: string | null, lastSeenAlive: string, isUp: boolean, name: string, friendlyName?: string | null, link: string }> };
+export type GetListOfBookmarksQuery = {
+  __typename?: 'query_root';
+  bookmarks: Array<{
+    __typename?: 'Bookmarks';
+    id: string;
+    link: string;
+    faviconUrl?: string | null;
+    displayName: string;
+    position?: number | null;
+  }>;
+  traefikRoutes: Array<{
+    __typename?: 'TraefikRoutes';
+    faviconUrl?: string | null;
+    lastSeenAlive: string;
+    isUp: boolean;
+    name: string;
+    friendlyName?: string | null;
+    link: string;
+  }>;
+};
 
 export type InsertBookmarkMutationVariables = Types.Exact<{
   object: Types.BookmarksInsertInput;
 }>;
 
-
-export type InsertBookmarkMutation = { __typename?: 'mutation_root', insertBookmarksOne?: { __typename?: 'Bookmarks', position?: number | null, link: string, id: string, faviconUrl?: string | null, displayName: string, userId: string } | null };
+export type InsertBookmarkMutation = {
+  __typename?: 'mutation_root';
+  insertBookmarksOne?: {
+    __typename?: 'Bookmarks';
+    position?: number | null;
+    link: string;
+    id: string;
+    faviconUrl?: string | null;
+    displayName: string;
+    userId: string;
+  } | null;
+};
 
 export const BookmarkItemFragmentDoc = gql`
-    fragment BookmarkItem on Bookmarks {
-  id
-  link
-  faviconUrl
-  displayName
-  position
-}
-    `;
-export const TraefikRoutesFragmentDoc = gql`
-    fragment TraefikRoutes on TraefikRoutes {
-  faviconUrl
-  lastSeenAlive
-  isUp
-  name
-  friendlyName
-  faviconUrl
-  link: calculatedUrl
-}
-    `;
-export const GetListOfBookmarksDocument = gql`
-    query getListOfBookmarks {
-  bookmarks(orderBy: {position: ASC_NULLS_LAST}) {
-    ...BookmarkItem
-  }
-  traefikRoutes(orderBy: {createdAt: DESC}, where: {isSown: {_eq: true}}) {
-    ...TraefikRoutes
-  }
-}
-    ${BookmarkItemFragmentDoc}
-${TraefikRoutesFragmentDoc}`;
-
-export function useGetListOfBookmarksQuery(options?: Omit<Urql.UseQueryArgs<GetListOfBookmarksQueryVariables>, 'query'>) {
-  return Urql.useQuery<GetListOfBookmarksQuery, GetListOfBookmarksQueryVariables>({ query: GetListOfBookmarksDocument, ...options });
-};
-export const InsertBookmarkDocument = gql`
-    mutation InsertBookmark($object: BookmarksInsertInput!) {
-  insertBookmarksOne(
-    object: $object
-    onConflict: {constraint: bookmarks_pkey, update_columns: displayName}
-  ) {
-    position
-    link
+  fragment BookmarkItem on Bookmarks {
     id
+    link
     faviconUrl
     displayName
-    userId
+    position
   }
+`;
+export const TraefikRoutesFragmentDoc = gql`
+  fragment TraefikRoutes on TraefikRoutes {
+    faviconUrl
+    lastSeenAlive
+    isUp
+    name
+    friendlyName
+    faviconUrl
+    link: calculatedUrl
+  }
+`;
+export const GetListOfBookmarksDocument = gql`
+  query getListOfBookmarks {
+    bookmarks(orderBy: { position: ASC_NULLS_LAST }) {
+      ...BookmarkItem
+    }
+    traefikRoutes(
+      orderBy: { createdAt: DESC }
+      where: { isSown: { _eq: true } }
+    ) {
+      ...TraefikRoutes
+    }
+  }
+  ${BookmarkItemFragmentDoc}
+  ${TraefikRoutesFragmentDoc}
+`;
+
+export function useGetListOfBookmarksQuery(
+  options?: Omit<Urql.UseQueryArgs<GetListOfBookmarksQueryVariables>, 'query'>
+) {
+  return Urql.useQuery<
+    GetListOfBookmarksQuery,
+    GetListOfBookmarksQueryVariables
+  >({ query: GetListOfBookmarksDocument, ...options });
 }
-    `;
+export const InsertBookmarkDocument = gql`
+  mutation InsertBookmark($object: BookmarksInsertInput!) {
+    insertBookmarksOne(
+      object: $object
+      onConflict: { constraint: bookmarks_pkey, update_columns: displayName }
+    ) {
+      position
+      link
+      id
+      faviconUrl
+      displayName
+      userId
+    }
+  }
+`;
 
 export function useInsertBookmarkMutation() {
-  return Urql.useMutation<InsertBookmarkMutation, InsertBookmarkMutationVariables>(InsertBookmarkDocument);
-};
+  return Urql.useMutation<
+    InsertBookmarkMutation,
+    InsertBookmarkMutationVariables
+  >(InsertBookmarkDocument);
+}
 
 /**
  * @param resolver a function that accepts a captured request and may return a mocked response.
@@ -85,11 +142,17 @@ export function useInsertBookmarkMutation() {
  *   )
  * })
  */
-export const mockGetListOfBookmarksQuery = (resolver: ResponseResolver<GraphQLRequest<GetListOfBookmarksQueryVariables>, GraphQLContext<GetListOfBookmarksQuery>, any>) =>
+export const mockGetListOfBookmarksQuery = (
+  resolver: ResponseResolver<
+    GraphQLRequest<GetListOfBookmarksQueryVariables>,
+    GraphQLContext<GetListOfBookmarksQuery>,
+    any
+  >
+) =>
   graphql.query<GetListOfBookmarksQuery, GetListOfBookmarksQueryVariables>(
     'getListOfBookmarks',
     resolver
-  )
+  );
 
 /**
  * @param resolver a function that accepts a captured request and may return a mocked response.
@@ -102,8 +165,14 @@ export const mockGetListOfBookmarksQuery = (resolver: ResponseResolver<GraphQLRe
  *   )
  * })
  */
-export const mockInsertBookmarkMutation = (resolver: ResponseResolver<GraphQLRequest<InsertBookmarkMutationVariables>, GraphQLContext<InsertBookmarkMutation>, any>) =>
+export const mockInsertBookmarkMutation = (
+  resolver: ResponseResolver<
+    GraphQLRequest<InsertBookmarkMutationVariables>,
+    GraphQLContext<InsertBookmarkMutation>,
+    any
+  >
+) =>
   graphql.mutation<InsertBookmarkMutation, InsertBookmarkMutationVariables>(
     'InsertBookmark',
     resolver
-  )
+  );
