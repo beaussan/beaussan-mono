138a1901789c052c6b74dfe79b7f0f1a46838ea9
Nicolas Beaussart <nic.beaussart@gmail.com>
2023-02-26 14:39:12 +0100

    update nx (#14)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,14 @@
 import { StorybookConfig as WebpackStorybookConfig } from '@storybook/react-webpack5';
 import { StorybookConfig as ViteStorybookConfig } from '@storybook/react-vite';
 
-type StorybookConfig = Omit<WebpackStorybookConfig, 'framework' | 'core'> &
+type StorybookConfig = Omit<
+  WebpackStorybookConfig,
+  'framework' | 'core' | 'stories'
+> &
   Pick<ViteStorybookConfig, 'viteFinal'>;
 
 export const rootMain: StorybookConfig = {
-  stories: [],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
-  ],
+  addons: ['@storybook/addon-interactions', '@storybook/addon-a11y'],
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
 
