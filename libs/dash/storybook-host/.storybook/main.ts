e292bdbb6d994c3f51a9a0a96a06959dd0275b33
Nicolas Beaussart <nic.beaussart@gmail.com>
2023-03-03 12:45:22 +0100

    fix storybook not running on ci

--- libs/dash/storybook-host/.storybook/main.ts
+++ libs/dash/storybook-host/.storybook/main.ts
@@ -1,15 +1,9 @@
 import { rootMain } from '../../../../.storybook/main';
 import type { StorybookConfig } from '@storybook/react-vite';
-import { mergeConfig } from 'vite';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 const config: StorybookConfig = {
   ...rootMain,
-  stories: [
-    ...rootMain.stories,
-    '../../**/*.stories.mdx',
-    '../../**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../../**/*.stories.mdx', '../../**/*.stories.@(js|jsx|ts|tsx)'],
 
   staticDirs: ['../../../../tools/msw'],
   addons: [
@@ -19,16 +13,11 @@ const config: StorybookConfig = {
   ],
   framework: {
     name: '@storybook/react-vite',
-    options: {},
-  },
-  viteFinal: (viteConfig, options) => {
-    return mergeConfig(viteConfig, {
-      plugins: [
-        viteTsConfigPaths({
-          root: '../../../../',
-        }),
-      ],
-    });
+    options: {
+      builder: {
+        viteConfigPath: 'libs/dash/storybook-host/vite.config.ts',
+      },
+    },
   },
 };
 
--- libs/shared/storybook-host/.storybook/main.ts
+++ libs/shared/storybook-host/.storybook/main.ts
@@ -1,16 +1,10 @@
 import { rootMain } from '../../../../.storybook/main';
 import type { StorybookConfig } from '@storybook/react-vite';
-import { mergeConfig } from 'vite';
-import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 const config: StorybookConfig = {
   staticDirs: ['../../../../tools/msw'],
   ...rootMain,
-  stories: [
-    ...rootMain.stories,
-    '../../**/*.stories.mdx',
-    '../../**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../../**/*.stories.mdx', '../../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-essentials',
     ...(rootMain.addons || []),
@@ -18,16 +12,11 @@ const config: StorybookConfig = {
   ],
   framework: {
     name: '@storybook/react-vite',
-    options: {},
-  },
-  viteFinal: (viteConfig, options) => {
-    return mergeConfig(viteConfig, {
-      plugins: [
-        viteTsConfigPaths({
-          root: '../../../../',
-        }),
-      ],
-    });
+    options: {
+      builder: {
+        viteConfigPath: 'libs/shared/storybook-host/vite.config.ts',
+      },
+    },
   },
 };
 export default config;
