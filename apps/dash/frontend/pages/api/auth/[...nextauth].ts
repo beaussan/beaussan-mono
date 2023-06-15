5212bc3bfcfd646f9224fc436a0760b580ea222e
Nicolas Beaussart <nic.beaussart@gmail.com>
2023-06-15 15:20:03 +0200

    force nx to detect nodemailer

--- apps/dash/frontend/pages/api/auth/[...nextauth].ts
+++ apps/dash/frontend/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,8 @@ import GithubProvider from 'next-auth/providers/github';
 import EmailProvider from 'next-auth/providers/email';
 import { CreateHasuraNextAuth } from '@beaussan/shared/utils/next-auth/hasura-adaptor';
 import { env } from '../../../lib/env';
-
+// Needed to kick nx dep detection to include nodemailer in the bundle
+import 'nodemailer';
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 
