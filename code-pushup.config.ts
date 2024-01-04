72edd616dcc3eba501c8f9ff33792bedf48e16ff
Nicolas Beaussart <nic.beaussart@gmail.com>
2024-01-04 22:00:57 +0100

    experment

---
+++ code-pushup.config.ts
@@ -0,0 +1,34 @@
+import defaultEslintPlugin from '@code-pushup/eslint-plugin';
+import type { CoreConfig } from '@code-pushup/models';
+
+const eslintPlugin = defaultEslintPlugin.eslintPlugin;
+
+const eslintConfigFromNxProjects =
+  defaultEslintPlugin.eslintConfigFromNxProjects;
+
+const config: CoreConfig = {
+  persist: {
+    outputDir: '.code-pushup',
+    filename: 'report',
+    format: ['json', 'md'],
+  },
+
+  plugins: [await eslintPlugin(await eslintConfigFromNxProjects())],
+
+  categories: [
+    {
+      slug: 'bug-prevention',
+      title: 'Bug prevention',
+      refs: [{ type: 'group', plugin: 'eslint', slug: 'problems', weight: 1 }],
+    },
+    {
+      slug: 'code-style',
+      title: 'Code style',
+      refs: [
+        { type: 'group', plugin: 'eslint', slug: 'suggestions', weight: 1 },
+      ],
+    },
+  ],
+};
+
+export default config;
